54519349750c4f0707b9b854d57a164143c68ea0
josepmartorell <you@example.com>
2022-06-15 10:51:53 +0200

    basic elements

---
+++ src/encapsuladores/Titular.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package encapsuladores;
+
+/**
+ *
+ * @author jtech
+ */
+public class Titular {
+    private String codigo;
+    private String nombre;
+    private double cuotaParticipacion;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getCuotaParticipacion() {
+        return cuotaParticipacion;
+    }
+
+    public void setCuotaParticipacion(double cuotaParticipacion) {
+        this.cuotaParticipacion = cuotaParticipacion;
+    }
+    
+}
